b6a7e775c249d0305a7cf566a8d6aaaa194d4b99
SweetCoding115 <moriahbuckridge96115@gmail.com>
2023-11-10 16:52:01 +0800

    looping uses v-for=" x in xx", :key is also used.

--- main.js
+++ main.js
@@ -3,10 +3,15 @@ const app = Vue.createApp({
         return {
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
-            inventory: 100,
+            inStock: true,
+            details: ['50% cotton', '30% wool', '20% polyester'],
             // solution
-            onSale: true
+            sizes: ['S', 'M', 'L', 'XL'],
             // solution
+            variants: [
+              { id: 2234, color: 'green' },
+              { id: 2235, color: 'blue' },
+            ]
         }
     }
 })
